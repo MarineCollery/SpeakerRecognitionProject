280549cccdc18dd50ebd78fc6cb4c5a4d64c3f2e
MarineCollery <marine.collery@gmail.com>
2018-05-18 12:48:10 +0200

    Extraction for Speaker trait solved

--- Features/features_extraction.sh
+++ Features/features_extraction.sh
@@ -21,5 +21,5 @@ file_name=$(basename $path_wav)
 file_name_witout_ext=${file_name%.*}
 
 
-/Users/Marine/Downloads/openSMILE-2.1.0/inst/bin/SMILExtract  -C /Users/Marine/Downloads/openSMILE-2.1.0/myconfig/MFCC12_E_D_A_Z_csv.conf  -I ${path_wav}  -O ${path_csv}/MFCC/${file_name_witout_ext}.csv
-/Users/Marine/Downloads/openSMILE-2.1.0/inst/bin/SMILExtract  -C /Users/Marine/Downloads/openSMILE-2.1.0/myconfig/Speaker_recognition.conf  -I ${path_wav}  -O ${path_csv}/Speaker_trait/results.csv
+# /Users/Marine/Downloads/openSMILE-2.1.0/inst/bin/SMILExtract  -C /Users/Marine/Downloads/openSMILE-2.1.0/myconfig/MFCC12_E_D_A_Z_csv.conf  -I ${path_wav}  -O ${path_csv}/MFCC/${file_name_witout_ext}.csv
+/Users/Marine/Downloads/openSMILE-2.1.0/inst/bin/SMILExtract  -C /Users/Marine/Downloads/openSMILE-2.1.0/myconfig/Speaker_recognition.conf  -I ${path_wav}  -O ${path_csv}/Speaker_trait/${file_name_witout_ext}.csv
