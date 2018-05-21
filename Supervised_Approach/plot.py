9a844c9b75be2676a1addca69719be27b89f7a15
Unknown <laabyounes@hotmail.com>
2018-05-21 16:58:36 +0200

    unsupervised avec average cluster purity
    fonction acp pour évaluer plus précisement la qualité des clusters et code plus propre

--- Supervised_Approach/plot.py
+++ Supervised_Approach/plot.py
@@ -38,38 +38,53 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.manifold import TSNE
 
+path_file = open("mypath.txt", "r")
+path = path_file.read()
+print(path)
+path_file.close()
 
+os.chdir(path)
 
 
 
 ## Plot data with PCA to 2:
 
-#df_train,df_test = data.data('MFCC', language="L2")
-df_train,df_test = data.data('Speaker_trait')
+df_train,df_test = data.data('MFCC')
+#df_train,df_test = data.data('Speaker_trait')
 
 df_train = df_train.fillna(0)#all NaN replaced by 0
 df_test = df_test.fillna(0)#all NaN replaced by 0
 
-
-df_test.sample(10000, randomState=17)
+df_train = df_train.sample(n=5000, random_state=17)
+df_test = df_test.sample(n=10000, random_state=17)
 
 dataset = data.restructure_data(df_train)
 dataset_test = data.restructure_data(df_test)
 
-dataset['data'] = dataset['data'].values.astype('float').append(dataset_test['data'].values.astype('float'))
-dataset['target'] = dataset['target'].values.astype('str').append(dataset_test['target'].values.astype('str'))
-dataset['language'] = dataset['language'].values.astype('str').append(dataset_test['language'].values.astype('str'))
-
+dataset['data'] = np.concatenate([dataset['data'].values.astype('float'), dataset_test['data'].values.astype('float')])
+dataset['target'] = np.concatenate([dataset['target'].values.astype('str'),
+                             dataset_test['target'].values.astype('str')])
+dataset['language'] = np.concatenate([dataset['language'].values.astype('str'),
+                             dataset_test['language'].values.astype('str')])
+print("Scaling")
 standard_scaler = StandardScaler()
 dataset['data'] = standard_scaler.fit_transform(dataset['data'])
 
-# reduce dimentionnality a bit if too high
-##dimRed = PCA(n_components=50)
-##dataset['data'] = dimRed.fit_transform(dataset['data'])
 
-dimRed = TSNE(n_components=2)
+print("Dataset size "+str(len(dataset['data'])))
+
+# reduce dimentionnality a bit if too high
+# dimRed = PCA(n_components=50)
+# dataset['data'] = dimRed.fit_transform(dataset['data'])
+print("dimentionnality reduction")
+dimRed = TSNE(n_components=2, verbose=1)
+#dimRed = PCA(n_components=2)
+
 
 dataset['data'] = dimRed.fit_transform(dataset['data'])
+
+print("dimentionnality reduction done")
+
 
 n = len(dataset['target'])
 dataset['target_speaker_language'] = []
@@ -79,8 +94,8 @@ for i in range(n):
 # m = len(set(dataset['target_speaker_language']))
 # indexType = list(set(dataset['target_speaker_language']))
 
-m = len(set(dataset['language']))
-indexType = list(set(dataset['language']))
+m = len(set(dataset['target_speaker_language']))
+indexType = list(set(dataset['target_speaker_language']))
 
 separatedData = [[] for i in range(m)]
 
@@ -93,10 +108,13 @@ for i in range(n):
     separatedData[typeSpeaker] += [dataset['data'][i]]
 
 
-colors = ['b','g','r','c','m','y','k','w']
+colors = ['b','g','r','c','m','y','k','orange']
 for j in range(len(indexType)):
     x = [elem[0] for elem in separatedData[j]]
     y = [elem[1] for elem in separatedData[j]]
     plt.plot(x, y,color=colors[j], marker='+', label=indexType[j], linestyle='')
     plt.legend()
 plt.show()
+
+#np.savez('TSNE-Speaker_trait-15000_frames(PCA-50).npz', data=dataset)
+#np.savez('TSNE-MFCC-15000_frames.npz', data=dataset)
